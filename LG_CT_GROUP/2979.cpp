8d492ce7417e1b358366f56d66b3223cb3439642
mangona109 <100344078+mangona109@users.noreply.github.com>
2023-07-14 22:35:08 +0900

    Create 2979.cpp

---
+++ LG_CT_GROUP/2979.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+using namespace std;
+
+int timeline[101];
+int main(void){
+	//freopen("baekjoon_input.txt","rb",stdin);
+	
+	int A,B,C; 
+	int start, end;
+	int cost=0;
+	scanf("%d %d %d",&A, &B, &C);
+	
+	for(int i=0; i<3; i++){
+		scanf("%d %d",&start, &end);
+		for(int i=start; i<end; i++){
+			timeline[i]+=1;
+		}
+	}
+	
+	
+	for(int i=1; i<=100; i++){
+		if(timeline[i]==1){
+		cost += A;	
+		}
+		else if(timeline[i]==2){
+		cost += 2*B;	
+		}
+		else if(timeline[i]==3){
+		cost += 3*C;	
+		}
+	}
+	
+	printf("%d",cost);
+	return 0;
+}
