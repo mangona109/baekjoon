517c84f2d7d2461ca8a388dafd6e1db292d0b27c
mangona109 <100344078+mangona109@users.noreply.github.com>
2023-07-17 00:29:54 +0900

    Create 2667 단지번호 붙이기.cpp

---
+++ LG_CT_GROUP/그래프/2667 단지번호 붙이기.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int map[51][51];
+
+int adj_x[4] = {0,0,-1,+1};
+int adj_y[4] = {+1,-1,0,0};
+
+int house = 0;
+int danji = 0;
+void DFS(int xx, int yy){
+	map[xx][yy]=0;
+	house+=1;
+	
+	for(int i=0; i<4; i++){
+		if(map[xx+adj_x[i]][yy+adj_y[i]]=='1'){
+			DFS(xx+adj_x[i],yy+adj_y[i]);
+		}
+	}
+}
+
+int main(void){
+	//freopen("baekjoon_input.txt","rb",stdin);
+	
+	int num;
+	int M,N,K;
+	int x,y;
+	vector<int> houses;
+	scanf("%d",&N);
+	for(int i=0; i<N; i++){
+		scanf("%s",map[i]);
+	}
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			if(map[i][j]=='1'){
+				DFS(i,j);
+				danji+=1;
+				houses.push_back(house);
+				house=0;
+			}
+		}
+	}
+	
+	sort(houses.begin(),houses.end());
+	printf("%d\n",danji);
+	for(auto i : houses){
+		printf("%d\n",i);
+	}
+	
+	
+	return 0;
+}
