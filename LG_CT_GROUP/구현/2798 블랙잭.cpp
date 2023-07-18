bf3668536a282bb240c1dac885fb193124d3181e
mangona109 <100344078+mangona109@users.noreply.github.com>
2023-07-18 11:42:20 +0900

    Create 2798 블랙잭.cpp
    result 초기화 안해서 계속 틀렸음. 변수 초기화 주의

---
+++ LG_CT_GROUP/구현/2798 블랙잭.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+using namespace std;
+
+int input[100];
+
+int main(void){
+	//freopen("baekjoon_input.txt","rb",stdin);
+	int N,M, sum, result=0;
+	scanf("%d %d",&N,&M);
+	
+	for(int i=0; i<N; i++){
+		scanf("%d", &input[i]);
+	}
+	
+	for(int i=0; i<N-2; i++){
+		for(int j=i+1; j<N-1; j++){
+			for(int k=j+1; k<N; k++){
+				sum = input[i] + input[j] + input[k];
+				if(sum<=M && sum>result){
+					result = sum;
+				}
+			}
+		}
+	}
+	
+	printf("%d",result);
+	return 0;
+}
