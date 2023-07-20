79b5bd18f470be9e9b899c83c9dbc9493560dae6
mangona109 <100344078+mangona109@users.noreply.github.com>
2023-07-20 22:55:48 +0900

    Create 2839 설탕배달.cpp

---
+++ 브루트포스/2839 설탕배달.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+  //freopen("baekjoon_input.txt","rb",stdin);
+	int N;
+	int five=0, three=0;
+	scanf("%d",&N);
+	
+	five = N/5;
+	N%=5;
+	while((N%3)!=0){
+		if(five==0) break;
+		N+=5;
+		five-=1;
+	}
+	
+	if(N%3==0){
+		three=N/3;
+		printf("%d",three+five);
+	}
+	else printf("-1");
+	
+	return 0;
+}
